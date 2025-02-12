fcdbea2b90f64316ac1c8c69c01badee48e22d18
Michelle Sarmiento <michellechicsarmiento@yahoo.com>
2025-02-12 09:17:22 +0100

    tailwind css, reusable components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import NewProject from "./components/NewProject";
+import NoSelectedProject from "./components/NoSelectedProject";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <main>
-      <Sidebar></Sidebar>
+    <main className="h-screen my-8 flex gap-8">
+      <Sidebar />
+      {/* <NewProject/> */}
+      <NoSelectedProject/>
     </main>
   );
 }
